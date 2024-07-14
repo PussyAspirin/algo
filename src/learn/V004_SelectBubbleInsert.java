cd4fa2630eb5c5474f75af27f67b9d75b79b2854
PussyAspirin <bull3hitking@gmail.com>
2024-07-14 11:20:52 +0800

    4 Select + Bubble + Insertion

--- src/learn/V004_SelectBubbleInsert.java
+++ src/learn/V004_SelectBubbleInsert.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class V004_SelectBubbleInsert {
     public static void main(String[] args) {
         int[] array1 = {1, 3, 4, 2, 5};
-        selectionSort(array1);
+        insertionSort(array1);
         System.out.println(Arrays.toString(array1));
     }
 
@@ -30,4 +30,32 @@ public class V004_SelectBubbleInsert {
         arr[i] = arr[j];
         arr[j] = temp;
     }
+    
+    //2. 冒泡排序
+    public static void bubbleSort(int[] arr) {
+        // 边界情况
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    swap(arr, j, j+1);
+                }
+            }
+        }
+    }
+    
+    // 3. 插入排序**
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int j = i;
+            while ( j > 0 && arr[j] < arr[j-1]) {
+                swap(arr, j, j-1);
+                j--;
+            }
+        }
+    }
+    
+    
 }
